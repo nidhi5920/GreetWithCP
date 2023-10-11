3133567e776a58b6da97c7f80363ea8fb9a6b7ba
nidhi5920 <nidhisharma9126@gmail.com>
2023-10-11 20:58:53 +0530

    initial commit

---
+++ Greet.xcframework/ios-arm64/Greet.framework/Headers/Greet.h
@@ -0,0 +1,18 @@
+//
+//  Greet.h
+//  Greet
+//
+//  Created by Nidhi Sharma on 10/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Greet.
+FOUNDATION_EXPORT double GreetVersionNumber;
+
+//! Project version string for Greet.
+FOUNDATION_EXPORT const unsigned char GreetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Greet/PublicHeader.h>
+
+
